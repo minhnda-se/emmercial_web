f9e1b6668179d16feef2c45a53ae0a344fa69d4e
minhnda-se <anhminh02111@gmail.com>
2025-04-01 22:06:29 +0700

    complete top deals

--- src/app/pages/Home/Home.jsx
+++ src/app/pages/Home/Home.jsx
@@ -12,6 +12,7 @@ import Link from "daisyui/components/link";
 import { Banner } from "./usecases/Banner";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
+import { RatingStar } from "./usecases/RatingStar";
 const content = [
   { title: "Content 1", description: "Some description here" },
   { title: "Content 2", description: "Another description" },
@@ -90,7 +91,7 @@ const Home = () => {
         <HomeSkeleton />
       ) : (
         <>
-          <div className="flex flex-col justify-between fixed bottom-2 right-2 bg-secondary rounded-lg p-4 w-20 h-25 z-10">
+          {/* <div className="flex flex-col justify-between fixed bottom-2 right-2 bg-secondary rounded-lg p-4 w-20 h-25 z-10">
             <button
               className="btn btn-ghost text-white h-1/2"
               aria-label="Message"
@@ -99,7 +100,7 @@ const Home = () => {
               className="btn btn-ghost text-white h-1/2"
               aria-label="Support"
             ></button>
-          </div>
+          </div> */}
 
           <div className="home_container flex">
             <div className="home_sidebar rounded-lg bg-white !sticky top-2 h-screen overflow-y-auto custom-scrollbar">
@@ -109,7 +110,7 @@ const Home = () => {
               <div className="h-70 bg-white rounded-lg shadow-md ">
                 <Banner banner={banner} />
               </div>
-              <div className="h-32 bg-white rounded-lg shadow-md flex justify-around items-center">
+              <div className="min-h-32 bg-white rounded-lg shadow-md flex justify-around items-center">
                 {hotDeals?.items?.map((item, index) => (
                   <button
                     key={index}
@@ -158,26 +159,84 @@ const Home = () => {
                   >
                     {topDeals?.items?.slice(0, 34).map((item, index) => (
                       <SwiperSlide key={item.id}>
-                        <div className="card shadow-sm  border-1 border-zinc-100">
-                          <figure>
+                        <div className="card shadow-sm  border-1 border-zinc-100 home-card">
+                          <figure className="relative">
                             <img
                               src={item.thumbnail_url}
                               alt="Shoes"
+                              className="w-full"
+                              style={{
+                                width: `${item.thumbnail_width}px`,
+                              }}
+                            />
+                            <img
+                              src={item.badges_v3[0].image}
+                              alt="Badge"
+                              className="absolute top-0 left-0 w-full"
                               style={{
                                 width: `${item.thumbnail_width}px`,
                               }}
                             />
                           </figure>
-                          <div className="card-body">
-                            <h2 className="card-title">Card Title</h2>
-                            <p>
-                              A card component has a figure, a body part, and
-                              inside body there are title and actions parts
-                            </p>
-                            <div className="card-actions justify-end">
-                              <button className="btn btn-primary">
-                                Buy Now
-                              </button>
+
+                          <div className="card-body !mt-3 !p-3 ">
+                            <h2 className="card-title text-[12px] h-9 truncate-multiline">
+                              {item.name}
+                            </h2>
+                            <RatingStar rating={item.rating_average} />
+                            <div className="h-17 flex flex-col">
+                              <h3
+                                className={`text-${
+                                  item.discount_rate !== 0
+                                    ? "secondary"
+                                    : "black"
+                                } font-bold text-[16px]`}
+                              >
+                                {item.price?.toLocaleString()}
+                                <span className="align-super underline text-[14px]">
+                                  đ
+                                </span>
+                              </h3>
+                              {item.discount_rate !== 0 ? (
+                                <div className="flex gap-1 items-start">
+                                  <div className="badge badge-neutral w-10 h-5 text-[10px] flex justify-center items-center">
+                                    -{item.discount_rate}%
+                                  </div>
+
+                                  <h3 className="text-[11px] line-through text-gray-500">
+                                    {item.price?.toLocaleString()}
+
+                                    <span className="align-super text-[8px]">
+                                      đ
+                                    </span>
+                                  </h3>
+                                </div>
+                              ) : (
+                                <div className="h-5"></div>
+                              )}
+                              {item.origin && (
+                                <p className="text-[10px] !mt-1 truncate-multiline h-2">
+                                  Made in {item.origin}
+                                </p>
+                              )}
+                            </div>
+                            <hr className="border-base-200 border-1" />
+                            <div className="flex justify-center items-center gap-1">
+                              <div
+                                className="rounded"
+                                style={{
+                                  width: `${item.badges_new[0].icon_width}px`,
+                                  height: `${item.badges_new[0].icon_height}px`,
+                                }}
+                              >
+                                <img
+                                  src={item.badges_new[0].icon}
+                                  alt="Tailwind-CSS-Avatar-component"
+                                />
+                              </div>
+                              <p className="text-[10px]">
+                                {item.badges_new[0].text}
+                              </p>
                             </div>
                           </div>
                         </div>
