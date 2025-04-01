387d208e91df2edf4f861b788f9361c3620656aa
ThienNgoHuong <thiennhse183982@fpt.edu.vn>
2025-04-01 19:07:21 +0700

    Ảnh product trong detail

--- src/app/routes/MainRoutes.jsx
+++ src/app/routes/MainRoutes.jsx
@@ -4,6 +4,8 @@ import Header from "../layouts/Header";
 import PageNotFound from "../layouts/PageNotFound";
 import Footer from "../layouts/Footer/Footer";
 import Home from "../pages/Home";
+import DetailPng from "../components/DetailPng/DetailPng";
+import Detail from "../pages/Detail/Detail";
 const MainRoutes = () => {
   return (
     <BrowserRouter>
@@ -11,6 +13,7 @@ const MainRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<PageNotFound />} />
+        <Route path="/detail" element={<Detail />} />
       </Routes>
       <Footer />
     </BrowserRouter>
